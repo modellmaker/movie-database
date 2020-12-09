d93218eb684cada979ebca94afa18eda9c88da9c
modellmaker <modellmaker01@gmail.com>
2020-12-09 09:39:40 +0100

    movie-pages

--- main.py
+++ main.py
@@ -1,54 +1,84 @@
 from flask import Flask, render_template, request, make_response
-from models import User, db
+from models import User, MovieDB, db
+import uuid
+import hashlib
 
 app = Flask(__name__)
+# db.drop_all()
 db.create_all()
 
 
 class Movies:
-    def __init__(self, title, summary, imdb):
+    def __init__(self, title, summary, imdb, rating, length, season, verdict):
         self.title = title
         self.summary = summary
         self.imdb = imdb
+        self.rating = rating
+        self.length = length
+        self.season = season
+        self.verdict = verdict
 
 
-egy = Movies("cim", "leiras", "IMDB")
-ketto = Movies("cim2", "leiras2", "IMDB2")
-harom = Movies("cim3", "leiras3", "IMDB3")
+'''
+egy = Movies("cim", "leiras", "IMDB", "1.1", "1:23:45", "1", "verdict")
+ketto = Movies("cim2", "leiras2", "IMDB2", "1.2", "2:00:00", "2", "verdict2")
+harom = Movies("cim3", "leiras3", "IMDB3", "1.3", "3:00:00", "3", "verdict2")
 movies = [egy, ketto, harom]
-my_dict = {}
+
+for movie in movies:
+    movie = MovieDB(title=movie.title, summary=movie.summary, imdb=movie.imdb, rating=movie.rating, length=movie.length,
+                    season=movie.season, verdict=movie.verdict)
+    db.add(movie)
+    db.commit()'''
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    session_token = request.cookies.get("session_token")
+    if session_token:
+        movies = db.query(MovieDB).all()
+        user = db.query(User).filter_by(session_token=session_token).first()
+        return render_template("index.html", movies=movies, name=user.username)
+    else:
+        return render_template("index.html")
+
+
+@app.route("/movies/<title>")
+def movielink(title):
+    movie = db.query(MovieDB).filter_by(title=title).first()
+    session_token = request.cookies.get("session_token")
+    if session_token:
+        user = db.query(User).filter_by(session_token=session_token).first()
+    return render_template("movie-detail.html", movie=movie, name=user.username)
 
 
 @app.route("/login", methods=["POST"])
 def login():
-    while True:
-        username = request.form.get("username")
-        password = request.form.get("password")
+    username = request.form.get("username")
+    password = hashlib.sha256(request.form.get("password").encode()).hexdigest()
 
-        try:
-            user = db.query(User).filter_by(username=username).first()
+    try:
+        user = db.query(User).filter_by(username=username).first()
 
-            if username == user.username and password == user.password:
-                response = make_response(render_template("index.html", movies=movies, name=username))
-                response.set_cookie("c_username", username)
-            else:
-                message = "We can't find You. :( Please check, that You insert the correct username and password."
-                response = render_template("index.html", message=message)
-
-            return response
-
-        except AttributeError:
+        if username == user.username and password == user.password:
+            session_token = str(uuid.uuid4())
+            user.session_token = session_token
+            db.add(user)
+            db.commit()
+            movies = db.query(MovieDB).all()
+            response = make_response(render_template("index.html", movies=movies, name=username))
+            response.set_cookie("session_token", session_token, httponly=True)
+        else:
             message = "We can't find You. :( Please check, that You insert the correct username and password."
             response = render_template("index.html", message=message)
 
-            return response
+        return response
 
+    except AttributeError:
+        message = "We can't find You. :( Please check, that You insert the correct username and password."
+        response = render_template("index.html", message=message)
 
+        return response
 
 
 @app.route("/logout", methods=["POST"])
@@ -61,12 +91,10 @@ def logout():
 @app.route("/signup", methods=["POST"])
 def signup():
     username = request.form.get("username")
-    password = request.form.get("password")
-    user = User(username=username, password=password)
+    password = hashlib.sha256(request.form.get("password").encode()).hexdigest()
+    user = User(username=username, password=password, session_token=None)
     db.add(user)
     db.commit()
-    # writefile(username, password)
-    # print(my_dict)
     message = "You have successfully registered on our page. You can now login."
 
     response = make_response(render_template("index.html", message=message))
--- models.py
+++ models.py
@@ -1,10 +1,26 @@
 import os
 from sqla_wrapper import SQLAlchemy
+from sqlalchemy.pool import StaticPool
 
-db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+# db = SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+
+db = SQLAlchemy(os.getenv("DATABASE_URL", 'sqlite:///localhost.sqlite'), connect_args={'check_same_thread': False},
+                poolclass=StaticPool)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String)
     password = db.Column(db.String)
+    session_token = db.Column(db.String)
+
+
+class MovieDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True)
+    summary = db.Column(db.String)
+    imdb = db.Column(db.String)
+    rating = db.Column(db.Float)
+    length = db.Column(db.String)
+    season = db.Column(db.Integer)
+    verdict = db.Column(db.String)
